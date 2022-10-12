a6c19cff86767c37e3e8dc41b1ede1d049d073b3
Daniel Jurkiewicz <38923739+daniel-jurkiewicz@users.noreply.github.com>
2022-10-12 23:53:07 +0200

    NF-02 first tailwind edits

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <p className="text-5xl font-bold text-red-400 text-center rounded-3xl border-8 border-green-700 mt-6 p-10 shadow-lg">
+                Siemano, Mordeczki!
+            </p>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
